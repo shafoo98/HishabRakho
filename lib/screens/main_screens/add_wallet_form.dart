a458886c67209484db8977ca54a4b67f689a74eb
unknown <shafinul98@protonmail.com>
2021-04-06 18:00:47 +0600

    Fixed some errors

--- lib/screens/main_screens/add_wallet_form.dart
+++ lib/screens/main_screens/add_wallet_form.dart
@@ -28,12 +28,14 @@ class _AddWalletFormState extends State<AddWalletForm> {
             height: size.height * 0.025,
           ),
           TextFormField(
+            maxLength: 50,
             decoration: InputDecoration(
+              prefixIcon: Icon(Icons.edit),
               hintText: 'Name of wallet or account',
               fillColor: Colors.white,
               filled: true,
               enabledBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.black, width: 2.0),
+                borderSide: BorderSide(color: Colors.black54, width: 1.0),
               ),
             ),
             validator: (value) => value.isEmpty
@@ -46,25 +48,78 @@ class _AddWalletFormState extends State<AddWalletForm> {
             },
           ),
           SizedBox(
-            height: size.height * 0.025,
+            height: size.height * 0.015,
+          ),
+          SizedBox(
+            height: size.height * 0.15,
+            child: TextFormField(
+              maxLength: 250,
+              expands: true,
+              minLines: null,
+              maxLines: null,
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.edit),
+                hintText: 'Describe your wallet or account in details',
+                fillColor: Colors.white,
+                filled: true,
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black54, width: 1.0),
+                ),
+              ),
+              validator: (value) => value.isEmpty
+                  ? 'Please enter a description' + 'for wallet or account'
+                  : null,
+              onChanged: (value) {
+                setState(() {
+                  _walletDescription = value;
+                });
+              },
+            ),
+          ),
+          SizedBox(
+            height: size.height * 0.015,
           ),
           TextFormField(
+            maxLines: 1,
+            keyboardType: TextInputType.number,
             decoration: InputDecoration(
-              hintText: 'Describe your wallet or account in details',
+              prefixIcon: Icon(Icons.monetization_on),
+              hintText: 'Enter the amount of your wallet or account',
               fillColor: Colors.white,
               filled: true,
               enabledBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.black, width: 2.0),
+                borderSide: BorderSide(color: Colors.black54, width: 1.0),
               ),
             ),
             validator: (value) => value.isEmpty
-                ? 'Please enter a description' + 'for wallet or account'
+                ? 'Please enter an amount' + 'for wallet or account'
                 : null,
             onChanged: (value) {
               setState(() {
-                _walletName = value;
+                _walletValue = int.parse(value);
               });
             },
+          ),
+          SizedBox(
+            height: size.height * 0.025,
+          ),
+          RaisedButton(
+            onPressed: () {
+              print(_walletName +
+                  ' ' +
+                  _walletDescription +
+                  ' ' +
+                  '$_walletValue');
+            },
+            elevation: 5.0,
+            child: Text(
+              'Add Wallet',
+              style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w100,
+                  color: Colors.black45),
+            ),
+            color: Colors.grey[200],
           ),
         ],
       ),
